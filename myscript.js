1d12d5fcc4b851bf9eb06c5e2c59b5e543187e8b
Shivani Bhatt <shiavni170>
2020-03-06 17:47:41 +0530

    forth commit

---
+++ myscript.js
@@ -0,0 +1,20 @@
+function myfunction()
+{
+	
+	var x=document.getElementById("demo")
+	x.style.fontSize="20px";
+	x.style.color="red"
+	var z=document.getElementById("heading1")
+	z.style.color="blue"
+	z.style.fontsize="30px"
+	var y=document.getElementById("text").value
+	nn=prompt("Enter your name:" +y)
+	alert(nn)
+	
+	var a=5
+var b=6
+c=a+b
+console.log(c)
+
+
+}
